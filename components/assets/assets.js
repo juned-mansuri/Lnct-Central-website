447a4c40095a19e99b74bc475ba1b8b8673c219e
juned-mansuri <0studyforair@gmail.com>
2025-06-15 14:34:07 +0530

    assets added

--- components/assets/assets.js
+++ components/assets/assets.js
@@ -1,7 +1,75 @@
 import logo from './LNCT-Logo.png';
-
+import a from './a.jpg';
+import ab from './ab.png';
+import ac from './ac.jpg';
+import achive from './achive.jpg';
+import ad from './ad.png';
+import ae from './ae.jpg';
+import af from './af.jpg';
+import ag from './ag.png';
+import ah from './ah.jpg';
+import b from './b.jpg';
+import c from './c.jpg';
+import d from './d.jpg';
+import e from './e.jpg';
+import f from './f.png';
+import g from './g.jpg';
+import h from './h.jpg';
+import i from './i.jpg';
+import j from './j.jpg';
+import k from './k.jpg';
+import l from './l.jpg';
+import lnct from './lnct.jpg';
+import m from './m.jpg';
+import n from './n.png';
+import o from './o.jpg';
+import p from './p.jpg';
+import q from './q.jpg';
+import r from './r.jpg';
+import s from './s.jpg';
+import t from './t.jpg';
+import u from './u.jpg';
+import v from './v.jpg';
+import w from './w.jpg';
+import x from './x.jpg';
+import y from './y.jpg';
+import z from './z.jpg';
 
 export const assets = {
-  logo: logo
-
-}
+  logo,
+  a,
+  ab,
+  ac,
+  achive,
+  ad,
+  ae,
+  af,
+  ag,
+  ah,
+  b,
+  c,
+  d,
+  e,
+  f,
+  g,
+  h,
+  i,
+  j,
+  k,
+  l,
+  lnct,
+  m,
+  n,
+  o,
+  p,
+  q,
+  r,
+  s,
+  t,
+  u,
+  v,
+  w,
+  x,
+  y,
+  z,
+};
